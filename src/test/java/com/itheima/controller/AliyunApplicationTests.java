7353a4273daec8e7ab1214968cc4d934ef0e217d
tzq <tzq.com>
2023-05-30 19:30:20 +0800

    Initial commit

---
+++ src/test/java/com/itheima/controller/AliyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
